b81890fd62878fb7de13207ba6b1a15dc34bcc49
Николай Ветошкин <nik88350759@yandex.ru>
2022-12-05 20:14:55 +0300

    dz4+

--- dz4.py
+++ dz4.py
@@ -1,19 +1,16 @@
 # Вычислить число Пи c заданной точностью d
 
 
-# from math import pi
+from math import pi
 
-# n = pi
-# print(n)
+def mask(num: float, accuracy: float):
 
-# # Формула Бэйли — Боруэйна — Плаффа
+    accuracy = str(accuracy)
+    accuracy = len(accuracy[accuracy.find('.')+1::])
+    return float(f'{pi:0.{accuracy}}')
 
-# n = 100
-# my_pi = sum(1/16**x*(4/(8*x + 1) - 2/(8*x + 4) - 1/(8*x + 5) - 1/(8*x + 6)) for x in range(n))
-
-# print(my_pi)
-
-
+d = input("введите число")
+print(mask(pi,d))
 
 
 ############################################################
@@ -35,55 +32,55 @@ print(f"Простые множители числа {old} приведены в
 ############################################################
 # Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
-# lst = list(map(int, input("Введите числа через пробел:\n").split()))
-# print(f"Исходный список: {lst}")
-# new_lst = []
-# [new_lst.append(i) for i in lst if i not in new_lst]
-# print(f"Список из неповторяющихся элементов: {new_lst}")
+lst = list(map(int, input("Введите числа через пробел:\n").split()))
+print(f"Исходный список: {lst}")
+new_lst = []
+[new_lst.append(i) for i in lst if i not in new_lst]
+print(f"Список из неповторяющихся элементов: {new_lst}")
 
 
 ############################################################
 # Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
 
 
-# import random
+import random
 
 
-# def write_file(st):
-#     with open('file33.txt', 'w') as data:
-#         data.write(st)
+def write_file(st):
+    with open('file33.txt', 'w') as data:
+        data.write(st)
 
 
-# def rnd():
-#     return random.randint(0,101)
+def rnd():
+    return random.randint(0,101)
 
 
-# def create_mn(k):
-#     lst = [rnd() for i in range(k+1)]
-#     return lst
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
     
 
-# def create_str(sp):
-#     lst= sp[::-1]
-#     wr = ''
-#     if len(lst) < 1:
-#         wr = 'x = 0'
-#     else:
-#         for i in range(len(lst)):
-#             if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-#                 wr += f'{lst[i]}x^{len(lst)-i-1}'
-#                 if lst[i+1] != 0:
-#                     wr += ' + '
-#             elif i == len(lst) - 2 and lst[i] != 0:
-#                 wr += f'{lst[i]}x'
-#                 if lst[i+1] != 0:
-#                     wr += ' + '
-#             elif i == len(lst) - 1 and lst[i] != 0:
-#                 wr += f'{lst[i]} = 0'
-#             elif i == len(lst) - 1 and lst[i] == 0:
-#                 wr += ' = 0'
-#     return wr
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
 
-# k = int(input("Введите натуральную степень k = "))
-# koef = create_mn(k)
-# write_file(create_str(koef))
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
